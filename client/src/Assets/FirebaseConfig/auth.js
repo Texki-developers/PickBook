49d8667fcc45a47473b99ff54ac3503b131649d1
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-05-19 12:26:21 +0530

    routes are changed

--- client/src/Assets/FirebaseConfig/auth.js
+++ client/src/Assets/FirebaseConfig/auth.js
@@ -7,7 +7,6 @@ const socialMediaAuth = (provider) => {
     .auth()
     .signInWithPopup(provider)
     .then(async(res) => {
-       
         var userData={
             uid:res.user.uid,
             name:res.user.displayName,
@@ -15,7 +14,6 @@ const socialMediaAuth = (provider) => {
             phone:res.user.phoneNumber,
             photo:res.user.photoURL
         };
-        
         var loginServer = await instance.post('users/login',userData)
 
     }).catch((err) => {
--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({amshen:'hello'})
+  res.json({amshen:'this is admin'})
 });
 
 module.exports = router;
