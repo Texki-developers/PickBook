ba188eea24c5183cf49a4d5e7574db01f00dbb62
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2021-05-20 13:57:54 +0530

    checked user login

--- Helpers/userHelpers.js
+++ Helpers/userHelpers.js
@@ -5,11 +5,21 @@ const collection = require('../config/collections')
 module.exports = {
 
     addUser:(data)=>{
-        return new Promise((resolve,reject)=>{
+        return new Promise(async(resolve,reject)=>{
 
-            db.get().collection(collection.USER_COLLECTION).insertOne(data).then(res=>{
-                resolve(res.ops[0])
-            })
+            var user = await db.get().collection(collection.USER_COLLECTION).find({uid:data.uid}).toArray()
+
+            if(user.length===-1){
+                db.get().collection(collection.USER_COLLECTION).insertOne(data).then(res=>{
+                    resolve(res.ops[0])
+                })
+
+            }else{
+                resolve(data)
+            }
+
+
+           
         })
     },
     addBook:(bookDetails) => {
