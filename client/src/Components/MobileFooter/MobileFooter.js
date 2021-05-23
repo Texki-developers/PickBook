127a3379bda2c33ea28c2597482a2d52535ddc0b
AmshenShanu07 <amshen.shanu07.ya@gmail.com>
2021-05-23 11:12:31 +0530

    mobile footer done

--- client/src/Components/MobileFooter/MobileFooter.js
+++ client/src/Components/MobileFooter/MobileFooter.js
@@ -5,13 +5,45 @@ import {RiAccountCircleFill} from 'react-icons/ri'
 import {BsSearch} from 'react-icons/bs'
 import './MobileFooter.scss'
 import MobileOptions from './MobileOptions';
+import {useDispatch,useSelector} from 'react-redux' 
+import instance from '../../Assets/server/instance'
+import Actions from '../../Assets/Essentials/EssentialAction';
+
 const MobileFooter = () => {
+
+    const essentials = useSelector(state=>state.essentials)
+    const dispatch = useDispatch();
+
+    const logout =async ()=>{
+
+         
+
+        if(window.confirm('Do you want to LogOut?')){
+           await instance.get('/logout').then(async res=>{
+               
+               if(res.status === 200){
+                   await dispatch(Actions.logout());
+                   alert('You logout sucessfully')
+               }else{
+                   alert('Somerthink went Worng!\nTry again later')
+                   
+               }
+               
+           })
+       }
+   }
+
     return (
         <div className="mobile-footer">
             <MobileOptions option="Search"  icon={<BsSearch/>}/>
             <MobileOptions option="Add books" click="/add-book" icon={<GrAdd/>}/>
             <MobileOptions option="Filter" click="filter" icon={<FaFilter/>}/>
-            <MobileOptions option="Account"  icon={<RiAccountCircleFill/>}/>
+            <MobileOptions option="Account" click={essentials.isLogin?'logout':null} 
+            icon={essentials.isLogin? 
+            <img src={essentials.userData.photo} 
+            
+            onClick={logout} alt="" /> :
+            <RiAccountCircleFill/>}/>
         </div>
     )
 }
