6f46dd0fe98b114dfc0851db8c59bc5ee7295fd5
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-05-23 15:17:51 +0530

    add book page is completly function now we can add only jpg,jpeg or png

--- client/src/Components/AddBook/Addbook.js
+++ client/src/Components/AddBook/Addbook.js
@@ -2,7 +2,7 @@ import React, { useEffect, useLayoutEffect, useState } from 'react'
 import './AddBook.scss'
 import { useDispatch, useSelector } from 'react-redux';
 import TextField from '@material-ui/core/TextField';
-import { makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import firebase from '../../Assets/FirebaseConfig/firebaseConfig'
 import instance from '../../Assets/server/instance';
 import NotLoggedIn from '../NotLoggedIn/NotLoggedIn';
@@ -10,6 +10,7 @@ import Actions from '../../Assets/Essentials/EssentialAction';
 import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
+import Message from '../Message/Message';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -29,9 +30,12 @@ const Addbook = () => {
   const [fileName, setFileName] = useState(null)
   const [file, setFile] = useState(null);
   const [url, setUrl] = useState(null)
+  const [message,setMessage] = useState(false)
+  const [storeStatus,setStoreStatus] = useState(false)
+  const [messageColor,setMessageColor] = useState('')
   const essentials = useSelector(state => state.essentials);
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     dispatch(Actions.addIconToggle());
   }, [dispatch]);
@@ -41,13 +45,33 @@ const Addbook = () => {
       dispatch(Actions.addIconToggle())
     };
   }, [dispatch])
-  
+
   const viewImage = (event) => {
     setFile(event.target.files[0]);
-    console.log("asdfjaksdjfasdddddddddddddddddddddddd",event.target.file[0]);
     var date = new Date();
-    setFileName(date.getTime() + "-" + date.getDate() + "-" + date.getFullYear() + "-" + date.getMonth() + "-")
-    setUrl(URL.createObjectURL(event.target.files[0]))
+
+    if (event.target.files[0].type === "image/jpg") {
+      setFileName(date.getTime() + "-" + date.getDate() + "-" + date.getFullYear() + "-" + date.getMonth() + "-")
+      setUrl(URL.createObjectURL(event.target.files[0]))
+      setMessage(false)
+      setMessageColor("red")
+    }
+    else if(event.target.files[0].type === "image/jpeg"){
+      setFileName(date.getTime() + "-" + date.getDate() + "-" + date.getFullYear() + "-" + date.getMonth() + "-")
+      setUrl(URL.createObjectURL(event.target.files[0]))
+      setMessage(false)
+      setMessageColor("red")
+    }
+    else if(event.target.files[0].type === "image/png"){
+      setFileName(date.getTime() + "-" + date.getDate() + "-" + date.getFullYear() + "-" + date.getMonth() + "-")
+      setUrl(URL.createObjectURL(event.target.files[0]))
+      setMessage(false)
+      setMessageColor("")
+    }
+    else{
+      setMessage("Only allows jpg or png format files.")
+      setMessageColor("red")
+    }
   }
 
   const handleInput = (event) => {
@@ -58,8 +82,9 @@ const Addbook = () => {
   };
 
   const storeImage = () => {
-    return new Promise( async (resolve, reject) => {
-      console.log("uploading file....");
+    return new Promise(async (resolve, reject) => {
+      setStoreStatus("Uploading file")
+      setMessage("Please Wait while uploading!")
       await firebase.storage()
         .ref(`cover-images/${fileName + file.name}`)
         .put(file)
@@ -74,10 +99,10 @@ const Addbook = () => {
               .then((url) => {
                 const bookInfo = {
                   ...bookDetails,
-                  imageURL:url,
-                  userId : essentials.userData.uid
+                  imageURL: url,
+                  userId: essentials.userData.uid
                 }
-                console.log(bookInfo);
+                setStoreStatus("File Uploaded")
                 resolve(bookInfo)
 
               })
@@ -87,75 +112,82 @@ const Addbook = () => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    storeImage().then((bookInfo) => {
-      console.log(bookInfo);
-      instance.post('/add-book',bookInfo).then((res)=>{
-        console.log(res.data.status);
+    if(!message){
+      storeImage().then((bookInfo) => {
+        console.log(bookInfo);
+        instance.post('/add-book', bookInfo).then((res) => {
+          setMessage("Book Added successfully")
+          console.log(res.data.status);
+        })
       })
-    })
+    }else{
+      setMessage("Can't upload this type of file..")
+    }
     
+
   }
 
 
   return (
     <div className="add_book_container">
-      {essentials.userData?
+      {message?<Message message={message} link="/" color={messageColor}/>:null}
+      {essentials.userData ?
         <>
-        <h2 className="heading_book">Add Book</h2>
-        <form className={classes.root} id="frm" autoComplete="off" onSubmit={handleSubmit}>
-          <div className="inputs-container photo_upload">
-            <input type="file" id="upload" onChange={viewImage} hidden />
-            <label style={{backgroundImage:`url(${url})`}} id="cover" for="upload" >ADD PHOTO</label>
-            <label id="title">Cover photo</label>
-            
-          </div>
-          <div className="inputs-container">
-            <TextField className="inputs" label="IBN Number" required name='ibn' onChange={handleInput} />
-            <TextField className="inputs" label="Author Name" required name='author' onChange={handleInput} />
-          </div>
-          <div className="inputs-container" >
-            <TextField className="inputs" label="Book title" required name='title' onChange={handleInput} />
-            <TextField className="inputs" label="Link to purchase" required name='link' onChange={handleInput} />
-          </div>
-          <div className="inputs_dropdown">
-            <div className="input_cent">
-              <InputLabel id="add_language">Language</InputLabel>
-              <Select labelId="add_language" onChange={handleInput} name='Language'>
-                {essentials.languages.map((d,i)=>(
-                <MenuItem value={d} key={i}>{d}</MenuItem>
-                ))}
-              </Select>
+      
+          <h2 className="heading_book">Add Book</h2>
+          <form className={classes.root} id="frm" autoComplete="off" onSubmit={handleSubmit}>
+            <div className="inputs-container photo_upload">
+              <input type="file" id="upload" onChange={viewImage} hidden />
+              <label style={{ backgroundImage: `url(${url})` }} id="cover" for="upload" >ADD PHOTO</label>
+              <label id="title">{storeStatus?storeStatus:"Cover photo"}</label>
+
             </div>
-            <div className="input_cent">
-              <InputLabel id="add_genere">Genres</InputLabel>
-              <Select
-                labelId="add_genere"
-                name='Genres'
-                onChange={handleInput}
-              >
-                {essentials.genres.map((d,i)=>(
-                  <MenuItem value={d} key={i}>{d}</MenuItem>
-                ))}
-              </Select>
+            <div className="inputs-container">
+              <TextField className="inputs" label="IBN Number" required name='ibn' onChange={handleInput} />
+              <TextField className="inputs" label="Author Name" required name='author' onChange={handleInput} />
             </div>
-            <TextField   type='number'
-              onChange={handleInput} label="year" required name='year' />
-  
-          </div >
-  
-          <div className="inputs-container " >
-            <TextField className="description" multiline rowsMax={4}
-              onChange={handleInput} label="Description" required name='description' />
-  
-          </div>
-          <div className="ab-button-container">
-  
-            <button className="ab-button" type="submit">Add Book</button>
-          </div>
-        </form>
+            <div className="inputs-container" >
+              <TextField className="inputs" label="Book title" required name='title' onChange={handleInput} />
+              <TextField className="inputs" label="Link to purchase" required name='link' onChange={handleInput} />
+            </div>
+            <div className="inputs_dropdown">
+              <div className="input_cent">
+                <InputLabel id="add_language">Language</InputLabel>
+                <Select labelId="add_language" onChange={handleInput} name='Language'>
+                  {essentials.languages.map((d, i) => (
+                    <MenuItem value={d} key={i}>{d}</MenuItem>
+                  ))}
+                </Select>
+              </div>
+              <div className="input_cent">
+                <InputLabel id="add_genere">Genres</InputLabel>
+                <Select
+                  labelId="add_genere"
+                  name='Genres'
+                  onChange={handleInput}
+                >
+                  {essentials.genres.map((d, i) => (
+                    <MenuItem value={d} key={i}>{d}</MenuItem>
+                  ))}
+                </Select>
+              </div>
+              <TextField type='number'
+                onChange={handleInput} label="year" required name='year' />
+
+            </div >
+
+            <div className="inputs-container " >
+              <TextField className="description" multiline rowsMax={4}
+                onChange={handleInput} label="Description" required name='description' />
+
+            </div>
+            <div className="ab-button-container">
+              <button className="ab-button" type="submit">Add Book</button>
+            </div>
+          </form>
         </>
-      :
-        <NotLoggedIn/>
+        :
+        <NotLoggedIn />
       }
     </div>
   )
