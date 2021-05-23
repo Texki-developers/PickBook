4426aa2f530d520d0d4a5f4ce59c66d40b518a9a
Muhsin Neyyathur <muhsinny333@gmail.com>
2021-05-23 18:04:35 +0530

    logout added

--- client/src/Components/MobileFooter/MobileFooter.js
+++ client/src/Components/MobileFooter/MobileFooter.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {GrAdd} from 'react-icons/gr';
 import {FaFilter} from 'react-icons/fa'
 import {RiAccountCircleFill} from 'react-icons/ri'
@@ -8,41 +8,23 @@ import MobileOptions from './MobileOptions';
 import {useDispatch,useSelector} from 'react-redux' 
 import instance from '../../Assets/server/instance'
 import Actions from '../../Assets/Essentials/EssentialAction';
+import {FiLogOut} from 'react-icons/fi'
 
 const MobileFooter = () => {
-
+    const [logoutBtn, setLogoutBtn] = useState(false)
     const essentials = useSelector(state=>state.essentials)
     const dispatch = useDispatch();
 
-    const logout =async ()=>{
-
-         
-
-        if(window.confirm('Do you want to LogOut?')){
-           await instance.get('/logout').then(async res=>{
-               
-               if(res.status === 200){
-                   await dispatch(Actions.logout());
-                   alert('You logout sucessfully')
-               }else{
-                   alert('Somerthink went Worng!\nTry again later')
-                   
-               }
-               
-           })
-       }
-   }
-
     return (
         <div className="mobile-footer">
-            {/* <MobileOptions option="Search"  icon={<BsSearch/>}/> */}
+            {essentials.userData&&<MobileOptions option="Logout"  click="logout" icon={<FiLogOut/>}/>}
             <MobileOptions option="Add books" click="/add-book" icon={<GrAdd/>}/>
             <MobileOptions option="Filter" click="filter" icon={<FaFilter/>}/>
             <MobileOptions option="Account" click={essentials.isLogin?'logout':null} 
+            
             icon={essentials.isLogin? 
             <img src={essentials.userData.photo} 
-            
-            onClick={logout} alt="" /> :
+             alt="" /> :
             <RiAccountCircleFill/>}/>
         </div>
     )
